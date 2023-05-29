800929703b2b11c9030c1ca250e7ed65966caba3
Rusty Nuts <133261361+RustyNutsRust@users.noreply.github.com>
2023-05-29 11:02:21 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>🤍 Built with glub by <a href="https://github.com/dirty-fisherman/Broadway" target="_blank">Dirty Fisherman</a></p>
-      <p className={styles.invisible}>Balmer is funded by Xibalbus</p>
     </footer>
   )
 }
