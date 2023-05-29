8f6e3cba1790d7b613bd8a2aebf34b475c24e51d
Rusty Nuts <133261361+RustyNutsRust@users.noreply.github.com>
2023-05-29 11:59:48 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img src={process.env.NEXT_PUBLIC_LOGO_URL} alt={process.env.NEXT_PUBLIC_NAME} />
+        <img src="logo.png" alt={process.env.NEXT_PUBLIC_NAME} />
       </div>
       
       {process.env.NEXT_PUBLIC_DISCORD_URL && <div className={styles.discord}>
