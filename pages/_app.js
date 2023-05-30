400da87097bd62e9ced0269af4150cf674132e08
Pwoinky <89384979+Pwoinky@users.noreply.github.com>
2023-05-30 09:34:43 +0100

    CSS styling

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '@styles/globals.css'
 function Application({ Component, pageProps }) {
   return (
     <>
-      <style dangerouslySetInnerHTML={{__html: `body { background-image: url("background.png"); box-shadow: 0 0 8px 8px white inset; background-position: center; background-repeat: no-repeat; background-size: cover;  }`}}></style>
+      <style dangerouslySetInnerHTML={{__html: `body { background: #ce422b background-image: url("background.png"); box-shadow: 0 0 8px 8px white inset; background-position: center; background-repeat: no-repeat; background-size: cover;  }`}}></style>
       <Component {...pageProps} />
     </>
   )
