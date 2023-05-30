2ee1aa5633d1221e438190d52974365f6d235d3d
Pwoinky <89384979+Pwoinky@users.noreply.github.com>
2023-05-30 10:10:15 +0100

    Footer alteration

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>🤍 Built with glub by <a href="https://github.com/dirty-fisherman/Broadway" target="_blank">Dirty Fisherman</a></p>
+      <p>Adapted for Rusty Nuts Rust by <a href="https://www.linkedin.com/in/glenn-logan/">Glenn Logan</a></p>
     </footer>
   )
 }
