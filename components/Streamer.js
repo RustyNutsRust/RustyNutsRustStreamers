e03ee14ed101e309eec23b6b4eed3c537cc87970
Pwoinky <89384979+Pwoinky@users.noreply.github.com>
2023-06-01 15:45:31 +0100

    Dev

--- components/Streamer.js
+++ components/Streamer.js
@@ -1,15 +1,12 @@
 import styles from './Streamer.module.css'
 
 export default function Streamer({ game, started_at, title, user_name, thumbnail_url, profile_image_url }) {
-  // Check if thumbnail_url and profile_image_url are defined before accessing the replace method
-  const backgroundImage = thumbnail_url ? thumbnail_url.replace('{width}', '640').replace('{height}', '360') : '';
-  const avatarImage = profile_image_url || '';
-
   return (
     <a className={styles.Streamer} href={`https://twitch.tv/${user_name}`} target="_blank">
-      <div className={styles.StreamerThumbnail} style={{ backgroundImage }} />
+      <div className={styles.StreamerThumbnail} style={{ backgroundImage: `url(${thumbnail_url.replace('{width}', '640').replace('{height}', '360')})` }} />
       <div className={styles.StreamerInfo}>
-        <span className={styles.StreamerAvatar} style={{ backgroundImage: `url(${avatarImage})` }} />
+        <span className={styles.StreamerAvatar} style={{ backgroundImage: `url(${profile_image_url})` }}>
+        </span>
         <div>
           <h1 className={styles.StreamerUser}>{user_name}</h1>
           <h2 className={styles.StreamerTitle}>{title}</h2>
@@ -17,5 +14,5 @@ export default function Streamer({ game, started_at, title, user_name, thumbnail
         </div>
       </div>
     </a>
-  );
+  )
 }
