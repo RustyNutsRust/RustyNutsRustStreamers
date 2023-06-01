04a4bd84a285104143335dc4dc304ae80b7f6f6c
Pwoinky <89384979+Pwoinky@users.noreply.github.com>
2023-06-01 15:39:23 +0100

    Dev

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Streamer from '@components/Streamer'
@@ -27,7 +28,7 @@ export default function Home() {
         setIsLoading(false);
       })
       .catch(error => console.log(error));
-  }, []);
+  }, []); 
 
   return (
     <div className="container">
@@ -40,18 +41,17 @@ export default function Home() {
         <Header />
         <div className="content">
           <div className='stream-grid'>
-            {!isLoading && data
-              .map((streamer) =>
-                <Streamer
-                  key={streamer.user_name}
-                  game={streamer.game_name}
-                  started_at={streamer.started_at}
-                  title={streamer.title}
-                  user_name={streamer.user_name}
-                  thumbnail_url={streamer.thumbnail_url}
-                  profile_image_url={streamer.profile_image_url}
-                />
-              )}
+            {!isLoading && data.map(streamer => (
+              <Streamer
+                key={streamer.user_login}
+                game={streamer.game_name}
+                started_at={streamer.started_at}
+                title={streamer.title}
+                user_name={streamer.display_name}
+                thumbnail_url={streamer.thumbnail_url}
+                profile_image_url={streamer.profile_image_url}
+              />
+            ))}
           </div>
         </div>
         <Footer />
@@ -59,3 +59,4 @@ export default function Home() {
     </div>
   )
 }
+
